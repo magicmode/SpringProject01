319e76282b5328807b81de6e6a5d21ea82282292
magicmode <magicmode0@gmail.com>
2016-09-18 04:52:17 +0900

    스프링 MVC 15 - MySQL Connector-Java ( ssl warning 해결 )

--- workspace/prj_sample_01/src/test/java/com/javateam/sampleprj01/MySQLConnectionTest.java
+++ workspace/prj_sample_01/src/test/java/com/javateam/sampleprj01/MySQLConnectionTest.java
@@ -10,7 +10,7 @@ public class MySQLConnectionTest {
 	private static final String DRIVER = 
 			"com.mysql.jdbc.Driver";
 	private static final String URL = 
-			"jdbc:mysql://127.0.0.1:3306/world";
+			"jdbc:mysql://127.0.0.1:3306/world?useSSL=false";
 	private static final String USER = 
 			"root";
 	private static final String PW = 
